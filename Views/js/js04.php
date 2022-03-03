13342767dbe8800fc484249ae3bee022b47bd042
Praiwan-ch <63160256@go.buu.ac.th>
2022-03-03 13:20:41 +0700

    first commit

--- Views/js/js04.php
+++ Views/js/js04.php
@@ -8,11 +8,11 @@
     <img  onmouseover = "Shadow(this)" src="https://c.tenor.com/i3Qc4TdcXwMAAAAC/duck-meme.gif" width = "150px" height = "150px"  onmouseout ="normal(this)">&emsp;
     <img  onmouseover = "Shadow(this)" src="https://images.uncyc.org/th/thumb/e/e1/%E0%B8%AD%E0%B8%B2%E0%B8%88%E0%B8%B2%E0%B8%A3%E0%B8%A2%E0%B9%8C%E0%B9%81%E0%B8%94%E0%B8%87.jpg/300px-%E0%B8%AD%E0%B8%B2%E0%B8%88%E0%B8%B2%E0%B8%A3%E0%B8%A2%E0%B9%8C%E0%B9%81%E0%B8%94%E0%B8%87.jpg" width = "150px" height = "150px" onmouseout ="normal(this)">
    <script>
-       function Shadow(Pic){
-           Pic.style.boxShadow = "10px 20px 30px black";
+       function Shadow(pic){
+           pic.style.boxShadow = "10px 20px 30px black";
        }
-       function normal(Pic){
-           Pic.style.boxShadow = "10px 20px 30px white";
+       function normal(pic){
+           pic.style.boxShadow = "10px 20px 30px white";
        }
    </script>
 </body>
